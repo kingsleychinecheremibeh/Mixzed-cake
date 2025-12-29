17650b9f19d4e11511887bc968a41de9b10926b5
kingsleychinecheremibeh <kingsleyibeh363@gmail.com>
2025-12-29 01:40:36 +0100

    Enable PWA auto-update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/global.css';
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    updateSW(true);
+    console.log('New version available; please refresh.');
+  },
+  onOfflineReady() {
+    console.log('App is ready to work offline.');
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
